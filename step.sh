633ffce98a8a48d90a0968dc90668912f7a62fd8
Vanessa Medeiros <vmedeiro@thoughtworks.com>
2019-03-21 16:43:27 -0300

    Output generated report to a file

--- step.sh
+++ step.sh
@@ -3,7 +3,6 @@ set -ex
 
 if [ -z "${linting_path}" ] ; then
   echo " [!] Missing required input: linting_path"
-
   exit 1
 fi
 
@@ -17,6 +16,26 @@ if [ -s "${lint_config_file}" ] ; then
   FLAGS=$FLAGS' --config '"${lint_config_file}"  
 fi
 
+report_file='swiftlint_report'
+
+case $reporter in
+    xcode|emoji)
+      report_file="${report_file}.txt"
+      ;;
+    markdown)
+      report_file="${report_file}.md"
+      ;; 
+    csv|html)
+      report_file="${report_file}.${reporter}"
+      ;;
+    checkstyle|junit)
+      report_file="${report_file}.xml"
+      ;; 
+    json|sonarqube)
+      report_file="${report_file}.json"
+      ;; 
+esac
+
 cd "${linting_path}"
 
-swiftlint lint --reporter "${reporter}" ${FLAGS}
+swiftlint lint --reporter "${reporter}" ${FLAGS} > "${reporter_path}/${report_file}"
